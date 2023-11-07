f92d3cf643e14fb496a4021bc79c03ccfec0effa
tina25n <nguyen.tina25@yahoo.com>
2023-11-07 11:14:05 -0800

    Delete C version of code

--- build/heartrate/CMakeFiles/heartrate.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for heartrate.
-# This may be replaced when dependencies are built.
--- build/heartrate/CMakeFiles/heartrate.dir/home/tina25n/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj.d
+++
@@ -1,2 +0,0 @@
-heartrate/CMakeFiles/heartrate.dir/home/tina25n/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj: \
- /home/tina25n/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
