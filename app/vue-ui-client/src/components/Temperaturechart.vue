66f92abd240d9f90e987baad4d8b999d242a3dee
ker1200 <60584013+ker1200@users.noreply.github.com>
2024-02-01 01:53:56 -0800

    changed temperature graph

--- app/vue-ui-client/src/components/Temperaturechart.vue
+++ app/vue-ui-client/src/components/Temperaturechart.vue
@@ -1,82 +1,47 @@
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
-            series: [76],
-            chartOptions: {
-                chart: {
-                    type: 'radialBar',
-                    offsetY: -20,
-                    sparkline: {
-                        enabled: true
+            chart: {
+                type: 'bar',
+                dataLabels: {
+                    enabled: true,
+                    dropShadow: {
+                        enabled: true,
+                        left: 2,
+                        top: 2,
+                        opacity: 0.5
                     }
-                },
-                plotOptions: {
-                    radialBar: {
-                        startAngle: -90,
-                        endAngle: 90,
-                        track: {
-                            background: "#e7e7e7",
-                            strokeWidth: '97%',
-                            margin: 5, // margin is in pixels
-                            dropShadow: {
-                                enabled: true,
-                                top: 2,
-                                left: 0,
-                                color: '#999',
-                                opacity: 1,
-                                blur: 2
-                            }
-                        },
-                        dataLabels: {
-                            name: {
-                                show: false
-                            },
-                            value: {
-                                offsetY: -2,
-                                fontSize: '22px'
-                            }
-                        }
-                    }
-                },
-                grid: {
-                    padding: {
-                        top: -10
-                    }
-                },
-                fill: {
-                    type: 'gradient',
-                    gradient: {
-                        shade: 'light',
-                        shadeIntensity: 0.4,
-                        inverseColors: false,
-                        opacityFrom: 1,
-                        opacityTo: 1,
-                        stops: [0, 50, 53, 91]
-                    },
-                },
-                labels: ['Average Results'],
+                }
             },
+            series: [{
+                data: [{
+                    x: 'Temperature',
+                    y: 10
+                }]
+            }]
         }
     },
     methods: {
         getTemp() {
             const path = 'http://127.0.0.1:5000/temperature';
             axios.get(path)
-            .then((res) => {
-                this.series[0] = res.data;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+                .then((res) => {
+                    this.series[0].data[0].y = res.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         },
     },
-    created: async function() {
+    created: async function () {
         this.getTemp();
 
         setInterval(function () {
             this.getTemp();
-        }.bind(this), 500); 
+        }.bind(this), 500);
     }
 }
 </script>
@@ -84,6 +49,6 @@ export default {
 <template>
     <v-card color="purple" width=250 class="mt-n3">
         <v-card-title class="text-center"> Temperature </v-card-title>
-        <apexchart type="radialBar" :options="chartOptions" :series="series"></apexchart>
+        <apexchart type="bar" :options="chart" :series="series"></apexchart>
     </v-card>
 </template>
