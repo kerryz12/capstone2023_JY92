73e6b92973e8189de1de6a48f08c78aaa9df2f97
ker1200 <kerryzhang12@gmail.com>
2024-02-11 22:23:15 -0800

    fixed shoulder imu firmware

--- firmware/IMU/imu_lying/main.py
+++ firmware/IMU/imu_lying/main.py
@@ -1,7 +1,13 @@
 import machine
 import utime
-from networking import *
+from lib.networking import *
+from micropython import const
+from lib.ble_advertising import decode_services, decode_name
+import struct
+import bluetooth
+import time
 
+# Patient states
 POS_NONE = 0
 POS_LYING = 1
 POS_SITTING = 2
@@ -19,6 +25,181 @@ server_address = (host, port)
 network_obj = Networking()
 network_obj.connect()
 network_obj.createTCPSocket(server_address)
+
+current_room = 1
+
+#Localization states
+_IRQ_CENTRAL_CONNECT = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3)
+_IRQ_GATTS_READ_REQUEST = const(4)
+_IRQ_SCAN_RESULT = const(5)
+_IRQ_SCAN_DONE = const(6)
+_IRQ_PERIPHERAL_CONNECT = const(7)
+_IRQ_PERIPHERAL_DISCONNECT = const(8)
+_IRQ_GATTC_SERVICE_RESULT = const(9)
+_IRQ_GATTC_SERVICE_DONE = const(10)
+_IRQ_GATTC_CHARACTERISTIC_RESULT = const(11)
+_IRQ_GATTC_CHARACTERISTIC_DONE = const(12)
+_IRQ_GATTC_DESCRIPTOR_RESULT = const(13)
+_IRQ_GATTC_DESCRIPTOR_DONE = const(14)
+_IRQ_GATTC_READ_RESULT = const(15)
+_IRQ_GATTC_READ_DONE = const(16)
+_IRQ_GATTC_WRITE_DONE = const(17)
+_IRQ_GATTC_NOTIFY = const(18)
+_IRQ_GATTC_INDICATE = const(19)
+_IRQ_GAP_RSSI_RESULT = const(20)
+
+_ADV_IND = const(0x00)
+_ADV_DIRECT_IND = const(0x01)
+_ADV_SCAN_IND = const(0x02)
+_ADV_NONCONN_IND = const(0x03)
+
+_UART_SERVICE_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+_UART_RX_CHAR_UUID = bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
+_UART_TX_CHAR_UUID = bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
+
+class BLESimpleCentral:
+    def __init__(self, ble):
+        self._ble = ble
+        self._ble.active(True)
+        self._ble.irq(self._irq)
+        self._reset()
+
+    def _reset(self):
+        self._name = None
+        self._addr_type = None
+        self._addr = None
+        self._scan_callback = None
+        self._conn_callback = None
+        self._read_callback = None
+        self._notify_callback = None
+        self._conn_handle = None
+        self._start_handle = None
+        self._end_handle = None
+        self._tx_handle = None
+        self._rx_handle = None
+
+    def _irq(self, event, data):
+        if event == _IRQ_SCAN_RESULT:
+            addr_type, addr, adv_type, rssi, adv_data = data
+            if adv_type in (_ADV_IND, _ADV_DIRECT_IND) and _UART_SERVICE_UUID in decode_services(adv_data):
+                self._addr_type = addr_type
+                self._addr = bytes(addr)
+                self._name = decode_name(adv_data) or "?"
+                self._rssi = rssi
+                self._ble.gap_scan(None)
+        elif event == _IRQ_SCAN_DONE:
+            if self._scan_callback:
+                if self._addr:
+                    self._scan_callback(self._addr_type, self._addr, self._name, self._rssi)
+                    self._scan_callback = None
+                else:
+                    self._scan_callback(None, None, None, None)
+        elif event == _IRQ_PERIPHERAL_CONNECT:
+            conn_handle, addr_type, addr = data
+            if addr_type == self._addr_type and addr == self._addr:
+                self._conn_handle = conn_handle
+                self._ble.gattc_discover_services(self._conn_handle)
+        elif event == _IRQ_PERIPHERAL_DISCONNECT:
+            conn_handle, _, _ = data
+            if conn_handle == self._conn_handle:
+                self._reset()
+        elif event == _IRQ_GATTC_SERVICE_RESULT:
+            conn_handle, start_handle, end_handle, uuid = data
+            print("service", data)
+            if conn_handle == self._conn_handle and uuid == _UART_SERVICE_UUID:
+                self._start_handle, self._end_handle = start_handle, end_handle
+        elif event == _IRQ_GATTC_SERVICE_DONE:
+            if self._start_handle and self._end_handle:
+                self._ble.gattc_discover_characteristics(self._conn_handle, self._start_handle, self._end_handle)
+            else:
+                print("Failed to find uart service.")
+        elif event == _IRQ_GATTC_CHARACTERISTIC_RESULT:
+            conn_handle, def_handle, value_handle, properties, uuid = data
+            if conn_handle == self._conn_handle and uuid == _UART_RX_CHAR_UUID:
+                self._rx_handle = value_handle
+            if conn_handle == self._conn_handle and uuid == _UART_TX_CHAR_UUID:
+                self._tx_handle = value_handle
+        elif event == _IRQ_GATTC_CHARACTERISTIC_DONE:
+            if self._tx_handle is not None and self._rx_handle is not None:
+                if self._conn_callback:
+                    self._conn_callback()
+            else:
+                print("Failed to find uart rx characteristic.")
+        elif event == _IRQ_GATTC_WRITE_DONE:
+            conn_handle, value_handle, status = data
+            print("TX complete")
+        elif event == _IRQ_GATTC_NOTIFY:
+            conn_handle, value_handle, notify_data = data
+            if conn_handle == self._conn_handle and value_handle == self._tx_handle:
+                if self._notify_callback:
+                    self._notify_callback(notify_data)
+        elif event == _IRQ_GAP_RSSI_RESULT:
+            conn_handle, rssi = data
+            if conn_handle == self._conn_handle:
+                print("RSSI:", rssi)
+
+    def is_connected(self):
+        return self._conn_handle is not None and self._tx_handle is not None and self._rx_handle is not None
+
+    def scan(self, callback=None):
+        self._addr_type = None
+        self._addr = None
+        self._scan_callback = callback
+        self._ble.gap_scan(2000, 30000, 30000)
+
+    def connect(self, addr_type=None, addr=None, callback=None):
+        self._addr_type = addr_type or self._addr_type
+        self._addr = addr or self._addr
+        self._conn_callback = callback
+        if self._addr_type is None or self._addr is None:
+            return False
+        self._ble.gap_connect(self._addr_type, self._addr)
+        return True
+
+    def disconnect(self):
+        if self._conn_handle is None:
+            return
+        self._ble.gap_disconnect(self._conn_handle)
+        self._reset()
+
+    def write(self, v, response=False):
+        if not self.is_connected():
+            return
+        self._ble.gattc_write(self._conn_handle, self._rx_handle, v, 1 if response else 0)
+
+    def on_notify(self, callback):
+        self._notify_callback = callback
+
+def on_scan(addr_type, addr, name, rssi):
+    if addr_type is not None:
+        global current_room
+    
+    if addr_type is not None:
+        #print(name,rssi)
+        if(name == "Room 1" and rssi > -65):
+            print("Inside",name)
+            current_room = 1
+        elif(name == "Room 2" and rssi > -73):
+            print("Inside",name)
+            current_room = 2
+        elif(name == "Room 1" and rssi <= -65 and current_room == 1):
+            print("Corridor")
+            current_room = 3
+        elif(name == "Room 2" and rssi <= -73 and current_room == 2):
+            print("Corridor")
+            current_room = 3
+        elif(current_room == 3):
+            print("Corridor")
+        else:
+            print("Inside Room",current_room)
+        #Need to onvert rssi to distance
+        #Distance = 10^((Measured Power - Instant RSSI)/(10*N))
+        #Measured Power: rssi at a distance of 1m (need to measure), normally N=2
+        #or find a rssi_threshold
+    else:
+        print("Nothing Found")
 
 # Global variables to store the IMU data
 # acceleration, gyroscope and angle data read from the IMU.
@@ -42,6 +223,7 @@ def DueData(inputdata):  # New core procedures, read the data partition, each re
     global acc
     global gyro
     global Angle
+    acc = get_acc(ACCData)
     for data in inputdata:  # Traversal the input data
         if FrameState == 0:  # When the state is not determined, enter the following judgment
             if data == 0x55 and Bytenum == 0:  # When 0x55 is the first digit, start reading data and increment bytenum
@@ -110,15 +292,19 @@ def get_acc(datahex):
     azl = datahex[4]
     azh = datahex[5]
     k_acc = 16.0
-    acc_x = (axh << 8 | axl) / 32768.0 * k_acc
-    acc_y = (ayh << 8 | ayl) / 32768.0 * k_acc
-    acc_z = (azh << 8 | azl) / 32768.0 * k_acc
+    acc_x = (int(axh) << int(8) | int(axl)) / 32768.0 * k_acc
+    acc_y = (int(ayh) << int(8) | int(ayl)) / 32768.0 * k_acc
+    acc_z = (int(azh) << int(8) | int(azl)) / 32768.0 * k_acc
     if acc_x >= k_acc:
         acc_x -= 2 * k_acc
     if acc_y >= k_acc:
         acc_y -= 2 * k_acc
     if acc_z >= k_acc:
         acc_z -= 2 * k_acc
+        
+    print(acc_x)
+    print(acc_y)
+    print(acc_z)
     return acc_x, acc_y, acc_z
 
 
@@ -130,9 +316,9 @@ def get_gyro(datahex):
     wzl = datahex[4]
     wzh = datahex[5]
     k_gyro = 2000.0
-    gyro_x = (wxh << 8 | wxl) / 32768.0 * k_gyro
-    gyro_y = (wyh << 8 | wyl) / 32768.0 * k_gyro
-    gyro_z = (wzh << 8 | wzl) / 32768.0 * k_gyro
+    gyro_x = (int(wxh) << int(8) | int(wxl)) / 32768.0 * k_gyro
+    gyro_y = (int(wyh) << int(8) | int(wyl)) / 32768.0 * k_gyro
+    gyro_z = (int(wzh) << int(8) | int(wzl)) / 32768.0 * k_gyro
     if gyro_x >= k_gyro:
         gyro_x -= 2 * k_gyro
     if gyro_y >= k_gyro:
@@ -143,6 +329,8 @@ def get_gyro(datahex):
 
 
 def get_angle(datahex):
+    global current_room
+    
     rxl = datahex[0]
     rxh = datahex[1]
     ryl = datahex[2]
@@ -150,9 +338,9 @@ def get_angle(datahex):
     rzl = datahex[4]
     rzh = datahex[5]
     k_angle = 180.0
-    angle_x = (rxh << 8 | rxl) / 32768.0 * k_angle
-    angle_y = (ryh << 8 | ryl) / 32768.0 * k_angle
-    angle_z = (rzh << 8 | rzl) / 32768.0 * k_angle
+    angle_x = float(int(rxh) << int(8) | int(rxl)) / 32768.0 * k_angle
+    angle_y = float(int(ryh) << int(8) | int(ryl)) / 32768.0 * k_angle
+    angle_z = float(int(rzh) << int(8) | int(rzl)) / 32768.0 * k_angle
     if angle_x >= k_angle:
         angle_x -= 2 * k_angle
     if angle_y >= k_angle:
@@ -162,8 +350,15 @@ def get_angle(datahex):
     return angle_x, angle_y, angle_z
 
 def main():
+    global current_room
     print("Serial is Opened:", uart.any())
+    ble = bluetooth.BLE()
+    central = BLESimpleCentral(ble)
+    not_found = False
+    
     while True:
+        central.scan(on_scan)
+        
         if uart.any() > 0:
             datahex = uart.read(33)
             if datahex and len(datahex) == 33:
@@ -172,24 +367,50 @@ def main():
                 current_angles = get_angle(AngleData)
                 angle_x, angle_y, angle_z = current_angles
                 
-                # Check the first condition
-                if 60 <= angle_y <= 90 :
-                    network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                # Check the nromal lying condition ( face up)
+                if -40 <= angle_y <= 0 :
+                    #network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    network_obj.sendTCPPacket("1 " + str(POS_LYING) + " " + str(current_room))
                     print("lying")
-                elif 60 <= angle_x <= 110 and angle_y < 60:
-                    network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    
+                #Check the normal lying but with tilting (face up)
+                elif -110 <= angle_x <= -60 and -60 <= angle_y <= 40:
+                    #network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    network_obj.sendTCPPacket("1 " + str(POS_LYING) + " " + str(current_room))
                     print("lying")
-                elif -180 <= angle_x <= -60 and angle_y < 60:
-                    network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    
+                #Check the lying on the left side
+                elif 120 <= angle_x <= 170 and -80 <= angle_y < -30:
+                    #network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    network_obj.sendTCPPacket("1 " + str(POS_LYING) + " " + str(current_room))
                     print("lying")
+                    
+                #Check the lying on the right side
+                elif -125 <= angle_x <= -70 and 0 <= angle_y <= 70 :
+                    #network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    network_obj.sendTCPPacket("1 " + str(POS_LYING) + " " + str(current_room))
+                    print("lying")
+                    
+                elif -180 <= angle_x <= -90 and -85 <= angle_y < -40 :
+                    #network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    network_obj.sendTCPPacket("1 " + str(POS_LYING) + " " + str(current_room))
+                    print("lying")
+                    
+                elif 100 <= angle_x <= 180 and -85 <= angle_y < -50 :
+                    #network_obj.sendTCPPacket("1 " + str(POS_LYING))
+                    network_obj.sendTCPPacket("1 " + str(POS_LYING) + " " + str(current_room))
+                    print("lying")
+                    
                 else:
-                    network_obj.sendTCPPacket("1 " + str(POS_NONE))
+                    #network_obj.sendTCPPacket("1 " + str(POS_NONE))
+                    network_obj.sendTCPPacket("1 " + str(POS_NONE) + " " + str(current_room))
                     print("not lying")
 
-        
-        utime.sleep_ms(50)  # Delay to prevent reading too quickly
+        utime.sleep_ms(100)  # Delay to prevent reading too quickly
 
 # Main execution
 if __name__ == '__main__':
     main()  # Call the main function
    
+
+
