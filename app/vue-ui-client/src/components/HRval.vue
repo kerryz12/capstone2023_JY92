d200bc70ec7fd54a6ebe2d7262e87b586513976c
ker1200 <60584013+ker1200@users.noreply.github.com>
2024-03-18 01:35:27 -0700

    removed some test code

--- app/vue-ui-client/src/components/HRval.vue
+++ app/vue-ui-client/src/components/HRval.vue
@@ -14,25 +14,21 @@ export default {
         getHR() {
             const path = 'http://127.0.0.1:5000/heartrate';
             axios.get(path)
-            .then((res) => {
-                if (this.calibration_counter > 4) this.heartrate = res.data;
-                else this.calibration_counter += 1;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-            if (this.calibration_counter > 2) this.heartrate = '180';
-            else this.calibration_counter += 1;
-
+                .then((res) => {
+                    if (this.calibration_counter > 4) this.heartrate = res.data;
+                    else this.calibration_counter += 1;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         },
     },
-    created: async function() {
+    created: async function () {
         this.getHR();
 
         setInterval(function () {
             this.getHR();
-        }.bind(this), 1000); 
+        }.bind(this), 1000);
     }
 };
 
@@ -40,10 +36,10 @@ export default {
 
 
 <template>
-    <v-card max-width = 275 class="ml-4 mt-4 px-1 pt-1">
+    <v-card max-width=275 class="ml-4 mt-4 px-1 pt-1">
         <v-row>
             <v-col no-gutters>
-                <v-img width = 43 :src = "HRsrc" class = "ml-2 mr-1"></v-img>
+                <v-img width=43 :src="HRsrc" class="ml-2 mr-1"></v-img>
             </v-col>
             <v-col no-gutters>
                 <v-row>
@@ -53,6 +49,6 @@ export default {
                     <v-card-title class="mx-auto mt-n6">{{ heartrate }}</v-card-title>
                 </v-row>
             </v-col>
-        </v-row> 
+        </v-row>
     </v-card>
 </template>
--- app/vue-ui-client/src/views/MainDash.vue
+++ app/vue-ui-client/src/views/MainDash.vue
@@ -1,90 +1,96 @@
-  <script>
-  import axios from 'axios';
-  import SpO2chart from '@/components/SpO2chart.vue';
-  import tempHRBR from '@/components/tempHRBR.vue';
-  import HRandBRchart from '@/components/HRandBRchart.vue';
-  import Temperaturechart from '@/components/Temperaturechart.vue';
-  import BodyPosition from '@/components/BodyPosition.vue';
-  import BodyDynamics from '@/components/BodyDynamics.vue';
-  import Location from '@/components/Location.vue';
-  import HRval from '@/components/HRval.vue';
-  import BRval from '@/components/BRval.vue';
-  import alert from '@/components/alerts/alert.vue';
-  
-  export default {
-    name: 'MainDash',
-    data() {
-        return {
-        };
-    },
-    methods: {
-        getMessage() {
-            const path = 'http://localhost:5001/';
-            axios.get(path)
-                .then((res) => {
-                this.msg = res.data;
-            })
-                .catch((error) => {
-                console.error(error);
-            });
-        },
+<script>
+import axios from 'axios';
+import SpO2chart from '@/components/SpO2chart.vue';
+import tempHRBR from '@/components/tempHRBR.vue';
+import HRandBRchart from '@/components/HRandBRchart.vue';
+import Temperaturechart from '@/components/Temperaturechart.vue';
+import BodyPosition from '@/components/BodyPosition.vue';
+import BodyDynamics from '@/components/BodyDynamics.vue';
+import Location from '@/components/Location.vue';
+import HRval from '@/components/HRval.vue';
+import BRval from '@/components/BRval.vue';
+import alert from '@/components/alerts/alert.vue';
 
-        pollAlert() {
-          if (this.$refs.hr.heartrate > 150) this.$refs.alert.showModal();
-        }
-    },
-    created() {
-        this.getMessage();
+constant HR_ALERT = 150
 
-        setInterval(function () {
-            this.pollAlert();
-        }.bind(this), 1000);
+export default {
+  name: 'MainDash',
+  data() {
+    return {
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5001/';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    components: { SpO2chart, tempHRBR, HRandBRchart, Temperaturechart, 
-      BodyPosition, Location, HRval, BRval, BodyDynamics, alert }
+
+    pollAlert() {
+      if (this.$refs.hr.heartrate > HR_ALERT) {
+        this.$refs.alert.showModal();
+      }
+    }
+  },
+  created() {
+    this.getMessage();
+
+    setInterval(function () {
+      this.pollAlert();
+    }.bind(this), 1000);
+  },
+  components: {
+    SpO2chart, tempHRBR, HRandBRchart, Temperaturechart,
+    BodyPosition, Location, HRval, BRval, BodyDynamics, alert
+  }
 };
-  </script>
+</script>
 
 <template>
-  <v-container class="align-center justify-center"> 
-    <v-card class="align-center justify-center" color ="blue-lighten-3"> 
-      <v-card-title class="align-center"> Prototype 
-        <v-btn style="float: right" > Reset Device</v-btn>
+  <v-container class="align-center justify-center">
+    <v-card class="align-center justify-center" color="blue-lighten-3">
+      <v-card-title class="align-center"> Prototype
+        <v-btn style="float: right"> Reset Device</v-btn>
       </v-card-title>
     </v-card>
-      <v-sheet color = "blue-lighten-5" rounded class="align-center justify-center">
-        <v-row no-gutters>
-          <v-col no-gutters>
-            <v-container class="ml-n2">
-              <v-row>
-                <v-col no-gutters>
-                  <HRval ref="hr"></HRval>
-                </v-col>
-                <v-col no-gutters>
-                  <BRval></BRval>
-                </v-col>
-              </v-row>
-            </v-container>
+    <v-sheet color="blue-lighten-5" rounded class="align-center justify-center">
+      <v-row no-gutters>
+        <v-col no-gutters>
+          <v-container class="ml-n2">
+            <v-row>
+              <v-col no-gutters>
+                <HRval ref="hr"></HRval>
+              </v-col>
+              <v-col no-gutters>
+                <BRval></BRval>
+              </v-col>
+            </v-row>
+          </v-container>
 
-            <v-container class="ml-n2">
-              <v-row>
-                <v-container>
-                  <tempHRBR></tempHRBR>
-                </v-container>
-              </v-row>
-            </v-container>
-          </v-col>
-          
-          <v-col no-gutters>
+          <v-container class="ml-n2">
+            <v-row>
+              <v-container>
+                <tempHRBR></tempHRBR>
+              </v-container>
+            </v-row>
+          </v-container>
+        </v-col>
+
+        <v-col no-gutters>
           <v-container class="mt-3 ml-n8">
             <v-row>
-              <v-container >
+              <v-container>
                 <SpO2chart></SpO2chart>
               </v-container>
             </v-row>
-           
+
             <v-row>
-              <v-container >
+              <v-container>
                 <Temperaturechart></Temperaturechart>
               </v-container>
             </v-row>
@@ -95,26 +101,26 @@
               </v-container>
             </v-row>
           </v-container>
-          
-          </v-col>
-       
-          <v-col no-gutters>
+
+        </v-col>
+
+        <v-col no-gutters>
           <v-container class="mt-3 ml-n4">
             <v-row>
-              <v-container >
+              <v-container>
                 <BodyPosition></BodyPosition>
               </v-container>
             </v-row>
             <v-row>
-              <v-container >
+              <v-container>
                 <BodyDynamics></BodyDynamics>
               </v-container>
             </v-row>
           </v-container>
-          </v-col>
-          
-        </v-row>
-      </v-sheet>
+        </v-col>
+
+      </v-row>
+    </v-sheet>
   </v-container>
 
   <alert ref="alert"></alert>
